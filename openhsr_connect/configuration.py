cf67310bdfbe7174140179b24988aae6f9b37dc3
Fabian Hauser <fabian@fh2.ch>
2016-10-24 09:32:33 +0200

    Revert "Replace pyyaml with pureyaml"
    This reverts commit e14fa760707b6ffd857a5841b90fe0a35a5d43cb.

--- openhsr_connect/configuration.py
+++ openhsr_connect/configuration.py
@@ -1,5 +1,5 @@
 import os
-import pureyaml
+import yaml
 import logging
 import getpass
 import keyring
@@ -120,7 +120,7 @@ def load_config(raise_if_incomplete=False):
 
     config = None
     with open(config_path, 'r') as f:
-        config = pureyaml.load(f)
+        config = yaml.load(f)
 
     # Verify if the password is in the keyring
     try:
