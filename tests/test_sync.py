e29e72bfe5b0266d45b362273f072d30ed7b3d8c
Robin Suter <suter.robin@gmail.com>
2016-11-08 19:55:29 +0100

    add more tests for sync

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -2,6 +2,7 @@ import pytest
 import os
 import string
 import random
+import builtins
 from openhsr_connect import sync, configuration
 
 TEST_FOLDER = 'sync-test'
@@ -9,7 +10,7 @@ TEST_FOLDER = 'sync-test'
 
 def remove_smb_tree(share, connection, path):
     for shared_file in connection.listPath(share, path):
-        if (shared_file.filename != '.' and shared_file.filename != '..'):
+        if shared_file.filename != '.' and shared_file.filename != '..':
             full_path = os.path.join(path, shared_file.filename)
             if shared_file.isDirectory:
                 remove_smb_tree(share, connection, full_path)
@@ -19,7 +20,7 @@ def remove_smb_tree(share, connection, path):
     connection.deleteDirectory(share, path)
 
 
-def generate_files(n):
+def generate_files(n, content):
     num = 1
     while num <= n:
         filename = "file%s.txt" % num
@@ -27,31 +28,32 @@ def generate_files(n):
             [random.choice(string.ascii_letters) for i in range(random.randrange(10, 5000))])
         with open(filename, 'w') as fp:
             fp.write(random_data)
-            fp.write('\noriginal content\n')
+            fp.write(content)
         yield filename
         num += 1
 
 
 def create_remote_structure(connection):
-    connection.createDirectory('scratch', 'sync-test/dir1')
-    connection.createDirectory('scratch', 'sync-test/dir2')
-    connection.createDirectory('scratch', 'sync-test/dir3')
+    connection.createDirectory('scratch', os.path.join(TEST_FOLDER, 'dir1'))
+    connection.createDirectory('scratch', os.path.join(TEST_FOLDER, 'dir2'))
+    connection.createDirectory('scratch', os.path.join(TEST_FOLDER, 'dir3'))
     for n in range(1, 4):
-        for filename in generate_files(n):
-            file = open(filename, 'rb')
-            path = 'sync-test/dir%s/%s' % (n, filename)
-            connection.storeFile('scratch', path, file)
+        for filename in generate_files(n, 'original content'):
+            with open(filename, 'rb') as file:
+                path = '%s/dir%s/%s' % (TEST_FOLDER, n, filename)
+                connection.storeFile('scratch', path, file)
             os.remove(filename)
 
 
 @pytest.fixture
 def connection(config):
-    connection = sync.smb_login(config)
-    connection.createDirectory('scratch', TEST_FOLDER)
-    yield connection
+    new_connection = sync.smb_login(config)
+    new_connection.createDirectory('scratch', TEST_FOLDER)
+    yield new_connection
     # tear down
-    remove_smb_tree('scratch', connection, 'sync-test')
-    connection.close()
+    remove_smb_tree('scratch', new_connection, TEST_FOLDER)
+    sync.remove_tree(TEST_FOLDER)
+    new_connection.close()
 
 
 @pytest.fixture
@@ -61,7 +63,7 @@ def config():
     config['sync']['repositories'] = {
         'test': {
             'local-dir': TEST_FOLDER,
-            'remote-dir': 'sync-test',
+            'remote-dir': TEST_FOLDER,
             'exclude': []
         }
     }
@@ -82,9 +84,54 @@ def assert_file_structure(repo_name, path):
     assert os.path.exists(os.path.join(path, 'dir3', 'file3.txt'))
 
 
-def test_sync(config, connection, monkeypatch):
+def assert_file_content(filename, test_string):
+    data = None
+    with open(filename, 'r') as fp:
+        data = fp.read()
+    assert data.endswith(test_string)
+
+
+def test_sync_basic(config, connection, monkeypatch):
     create_remote_structure(connection)
     monkeypatch.setattr(sync, 'SMB_SHARE_NAME', 'scratch')
     sync.sync(config)
     assert_file_structure('test', TEST_FOLDER)
-    sync.remove_tree(TEST_FOLDER)
+
+
+def test_local_changes_ask_yes(config, connection, monkeypatch):
+    config['sync']['conflict-handling']['local-changes'] = 'ask'
+    create_remote_structure(connection)
+    monkeypatch.setattr(sync, 'SMB_SHARE_NAME', 'scratch')
+    sync.sync(config)
+    assert_file_structure('test', TEST_FOLDER)
+    # change local file
+    with open(os.path.join(TEST_FOLDER, 'dir1/file1.txt'), 'a') as fp:
+        fp.write('local change')
+    # nothing has changed on the remote, therefore there should not be a prompt here
+    sync.sync(config)
+    # Change remote file
+    for filename in generate_files(1, 'remote change'):
+        with open(filename, 'rb') as file:
+            path = '%s/dir%s/%s' % (TEST_FOLDER, 1, filename)
+            connection.storeFile('scratch', path, file)
+
+    # Now it should ask
+    monkeypatch.setattr(builtins, 'input', lambda x: "y")  # answer 'yes'
+    sync.sync(config)
+    assert_file_content(os.path.join(TEST_FOLDER, 'dir1/file1.txt'), 'remote change')
+
+
+def test_remote_deleted_ask_yes(config, connection, monkeypatch):
+    config['sync']['conflict-handling']['remote-deleted'] = 'ask'
+    create_remote_structure(connection)
+    monkeypatch.setattr(sync, 'SMB_SHARE_NAME', 'scratch')
+    sync.sync(config)
+    assert_file_structure('test', TEST_FOLDER)
+
+    remove_smb_tree('scratch', connection, os.path.join(TEST_FOLDER, 'dir1'))
+    remove_smb_tree('scratch', connection, os.path.join(TEST_FOLDER, 'dir2'))
+    monkeypatch.setattr(builtins, 'input', lambda x: "y")  # answer 'yes'
+    sync.sync(config)
+
+    assert not os.path.exists(os.path.join(TEST_FOLDER, 'dir1'))
+    assert not os.path.exists(os.path.join(TEST_FOLDER, 'dir2'))
