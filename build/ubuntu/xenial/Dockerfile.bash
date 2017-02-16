27bd7a59510d0c179ba56e46e1a6fd2e62709dde
Fabian Hauser <fabian@fh2.ch>
2017-02-16 10:36:48 +0100

    Add ubuntu xenial provision script

---
+++ build/ubuntu/xenial/Dockerfile.bash
@@ -0,0 +1,21 @@
+#!/bin/
+set -e
+# Required env variables:
+# - DOCKER_UID: UID of your desktop user
+# - DOCKER_GID: GID of your desktop user
+
+# User
+groupadd -g ${DOCKER_GID} user
+useradd --home /build -u ${DOCKER_UID} -g ${DOCKER_GID} -M user
+mkdir -p /source/{dist,build,openhsr_connect.egg-info} /repo /build/.gnupg
+chmod 700 /build/.gnupg
+chown -R ${DOCKER_UID}:${DOCKER_GID} /buil /repo /source
+
+# Packages
+apt-get update
+
+# Python
+apt-get install -y python3 python3-pip python3-pkg-resources
+
+# Packaging
+apt-get install -y dpkg-dev dpkg-sig reprepro
