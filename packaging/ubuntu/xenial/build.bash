0ab855802d95a7b5a89d96685c7e6f33cce75a50
Raphael Zimmermann <mail@raphael.li>
2017-02-27 21:36:37 +0100

    Package pysmb as well

--- packaging/ubuntu/xenial/build.bash
+++ packaging/ubuntu/xenial/build.bash
@@ -4,6 +4,15 @@ set -e
 # Import GPG signing key
 gpg --import <(echo -e "${GPG_KEY}")
 
+# get pysmb
+PYSMB_VERSION=1.1.19
+curl -o /build/pysmb-${PYSMB_VERSION}.tar.gz https://pypi.python.org/packages/f9/e7/1fd7faaa946cc6b43ce85bb7a177b75a4718d9c5e291201fec00112b497c/pysmb-1.1.19.tar.gz
+tar -xzf /build/pysmb-${PYSMB_VERSION}.tar.gz -C /build/
+cp -R /build/openhsr-connect/packaging/${DISTRIBUTION}/${VERSION}/pysmb_debian /build/pysmb-${PYSMB_VERSION}/debian/
+cd /build/pysmb-${PYSMB_VERSION}/
+sed -i 's/PYSMB_VERSION/'${PYSMB_VERSION}'/g' debian/changelog
+debuild --no-tgz-check
+
 # Build .deb and sign it... 
 cd /build/openhsr-connect/
 
@@ -14,23 +23,11 @@ cp -R packaging/${DISTRIBUTION}/${VERSION}/debian/ debian/
 debuild --no-tgz-check
 
 # Re-Generate pool
-rm -Rf /repo/*
 mkdir -p /repo/conf
 
-cat <<'__EOF__' > /repo/conf/distributions
-Origin: pool.openhsr.ch
-Label: openHSR Ubuntu Xenial Pool
-Suite: xenial
-Codename: xenial
-Version: 16.04
-Architectures: i386 amd64 source
-Components: main
-Description: openHSR Ubuntu Xenial Pool
-SignWith: Yes
-__EOF__
+cat /build/openhsr-connect/packaging/${DISTRIBUTION}/*/distributions > /repo/conf/distributions
 
 reprepro -Vb /repo/ export
-reprepro -Vb /repo/ includedeb xenial /build/openhsr-connect_${CONNECT_VERSION}_all.deb
-
-
-
+reprepro -Vb /repo/ includedeb ${VERSION} /build/openhsr-connect_${CONNECT_VERSION}_all.deb
+reprepro -Vb /repo/ includedeb ${VERSION} /build/python3-pysmb_${PYSMB_VERSION}_all.deb
+/bin/bash
